593335ab902f4ee4687e74c72cfa544999290c87
manato <manato20021113@gmail.com>
2024-04-25 23:01:41 +0900

    クエリパラメータで曲名とアーティスト名の間のみ+がつくように修正

--- app/app.py
+++ app/app.py
@@ -7,11 +7,12 @@ from spotipy.oauth2 import SpotifyClientCredentials
 import sys
 import json
 import re
+from urllib.parse import quote_plus
 
 username = '6w0j4q09g6iki4c318olak6a4' 
 my_id ='eed84f1ca1b2496fb69628e871630668' 
 my_secret = '381e0a9c2b95490d82353d755c1d600f' 
-token = "BQAJuFkB4naZUSDPg8-d5-krObb6vrQMq44McFEQ-9mzcC4L0-Xk-uiPTcVg-4FsAQEQFAKtJbSXUDLzDnBnOm1XhzZlvdWaMb0urWn9ZNKV2Bq2zyZ3xoTO3sXhZEZ1kbu6YFeGjEhhBAhg4W0VXeG69y3e603_b22DjeLwsCuQTKr30Wd1kAsxsCM_2Ht7et602KElpXB5snyPBN9boASB_oj3soW5alxBjw"
+token = "BQBLsKCWcjWOiMWgrwUhK4ga8wTrWJyFGadB2sgTohsbaTLod4KX2_2vgFfEciI2s9CHHrkeAvAHDxbJw9wsA4g3tIiZWV9TKNyPKLCCjgDGiEq5Z3sYtn2o439Xnz4xjnPliRPhQP3DM60PGdK2HiaTw4D_6_ja6wR551q2rqlyi_6UmxHv_wdSDNQEdTzQKr6p4IC6ZtzdFy9O6gfTs3eLTfeZTww5-WxALRPv_IIbC0gUKF9jrVj7HSiGfQAxc2c"
 spotify = spotipy.Spotify(auth = token)
 
 
@@ -25,7 +26,7 @@ def submit_setlist():
 
        id_part = url[last_hyphen_index+1:dot_html_index]
        
-       #create_playlist()
+      #  create_playlist()
        #仮想マシンの8000番をmacの8000番と紐づけているため、仮想マシン上のCLIでこのコードを実行する場合は、urlにローカルホスト（仮想マシン）の8000番を指定しないといけない
        url = f"http://0.0.0.0:8000/setlists/{id_part}"
        headers = {
@@ -37,22 +38,22 @@ def submit_setlist():
       
        for key in data['songs']:
         st.write(key['name']) # nameのみ参照
+        add_playlist(key["name"], key['original_artist'])
 
        return data
        
 def create_playlist():
    spotify.user_playlist_create(username, "myplaylist", public=False)
 
-#def add_playlist():
-
-   
+def add_playlist(name: str, artist: str):
+   q = f"{quote_plus(name)} {quote_plus(artist)}"
+   data = spotify.search(q, limit=1, offset=0, type='track', market="US")
+   st.write(data)
 
 def main():
    st.title("プレイリスト作成アプリ")
    st.write('このアプリはsetlist.fm(https://www.setlist.fm/)のURLからSpotifyのプレイリストを作成するアプリです')
    submit_setlist()
-   create_playlist()
-
 
 if __name__ == "__main__":
    main()
