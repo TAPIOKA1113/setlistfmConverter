e4932e322cb86ecfa1dcf61c8987bbdadc800949
manato <manato20021113@gmail.com>
2024-04-26 13:08:47 +0900

    bot用アカウントを使用するよう修正

--- app/app.py
+++ app/app.py
@@ -8,17 +8,18 @@ import sys
 import json
 import re
 from urllib.parse import quote_plus
+import streamlit.components.v1 as components
 
-username = '6w0j4q09g6iki4c318olak6a4' 
-my_id ='eed84f1ca1b2496fb69628e871630668' 
-my_secret = '381e0a9c2b95490d82353d755c1d600f' 
-token = "BQC-lDGOpx8dqIwy26HJfTiOn9hoAk-ECFk8YuoM1GElCQ04FieguGcxxKSd6SBcqcT7-kPV_mqQco2nYMTTLX8iI0oL_Oqo1Ri_s1owCyw3oEdOsDSe7j1Pxf7mm8_3FbHJhTgiJTE6oF49H2IKRdEr9zL0YsGZxDUtM-MPAoi_IhiwYwcwGR6jvMX601euty4NAo8mqn8TwVQNQdV3iKCwoVZb5wHA5twxB2jAVQCHdFaeOuIOav9ybeMKspSkVJA"
+username = 'gnti7y5zkih9elje0lzd4b84g' 
+my_id ='7ca33bfaf9ce41fbbc43a2abeec4e53d' 
+my_secret = '79b0572f34084761b508cbca34bd3512' 
+token = "BQCcPdiOLN5U0AEIlG77vptm0J1_uBbMBsgXxP9W8O9N4jcG2-CaffzwBrm9CeMarsco1Oisoyx9iZf-7wBbgJLAfEpXLTr4EsM8K5FLuZ3saGJVqCh4qDou5GNspCx2NJLBUp2-gKj2vNTg8NgxUqr_03izPHPQIVQeHx0eKDs-KfhdYGwQmr3NXWD9lzbtiMvlJypYgBDtLVdGYVdQnEljVpEeYLtl2Q"
 spotify = spotipy.Spotify(auth = token)
 
 
 def submit_setlist():
-   url = st.text_input("URLを入力してください")
-   submit = st.button("送信")
+   url = st.text_input("URLを入力", placeholder="https://www.setlist.fm/")
+   submit = st.button("作成")
    if submit:
        # ここで入力されたURLを処理する
        last_hyphen_index = url.rfind("-")
@@ -37,23 +38,26 @@ def submit_setlist():
        data = response.json()
       
        for key in data['songs']:
-        st.write(key['name']) # nameのみ参照
+        #st.write(key['name']) # nameのみ参照
         track_id = search_song(key["name"], key['original_artist'])
         add_playlist(playlist_id, track_id)
+       
+
+       components.iframe("https://open.spotify.com/embed/playlist/" + playlist_id , height=500)
 
        return data
        
 def create_playlist():
-   data = spotify.user_playlist_create(username, "myplaylist", public=False)
-   st.write(data['id'])
+   data = spotify.user_playlist_create(username, "myplaylist", public=True)
+   #st.write(data['id'])
    return data['id']  #プレイリストIDを返す
    
 
 def search_song(name: str, artist: str):
    q = f"{quote_plus(name)} {quote_plus(artist)}"
    data = spotify.search(q, limit=1, offset=0, type='track', market="US")
-   st.write(data['tracks']['items'][0]['id'])
-   st.write(data)
+   #st.write(data['tracks']['items'][0]['id'])
+   #st.write(data)
    return data['tracks']['items'][0]['id'] #トラックURIを返す
    
 
@@ -62,8 +66,9 @@ def add_playlist(playlist_id: str, track_id: str):
 
 def main():
    st.title("プレイリスト作成アプリ")
-   st.write('このアプリはsetlist.fm(https://www.setlist.fm/)のURLからSpotifyのプレイリストを作成するアプリです')
+   st.write('このアプリは[setlist.fm](https://www.setlist.fm/)のURLからSpotifyのプレイリストを作成するアプリです')
    submit_setlist()
+  
 
 if __name__ == "__main__":
    main()
